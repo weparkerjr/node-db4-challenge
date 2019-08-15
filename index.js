57619dcdeedee54abdfc49a636a8bb4cc441d82b
william-parker <edwinious@gmail.com>
2019-08-15 14:43:24 -0400

    Server Up and Running

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config(); // load .env variables
+
+const server = require('./server.js');
+
+const port = process.env.PORT || 3300;
+server.listen(port, () => {
+console.log(`\n*** Server listening on port ${port}***\n`)});
